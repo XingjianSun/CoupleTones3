32ab67705a0c36400343bc7dec9f6f03df2a4d58
fugongyuallen <gofu@ucsd.edu>
2016-06-03 16:39:28 -0700

    upload tone files with strategy pattern

---
+++ app/src/main/java/com/example/cody/coupletones/tones/whistleTone.java
@@ -0,0 +1,29 @@
+package com.example.cody.coupletones.tones;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.os.Vibrator;
+
+import com.example.cody.coupletones.R;
+
+/**
+ * Created by allenF on 6/3/16.
+ */
+public class whistleTone implements Tones {
+
+    MediaPlayer player;
+    Vibrator vibrate;
+    long[]vibrationPattern;
+
+    public whistleTone(String toneName, Vibrator vibrator){
+        vibrationPattern = new long[]{ 0, 100, 400, 100, 400, 100 };
+        vibrate = vibrator;
+    }
+    @Override
+    public void play(Context context) {
+
+        player = MediaPlayer.create(context, R.raw.inception);
+        player.start();
+
+    }
+}
